aa358c09ef26287f8593a83786662b03da696dbd
abrooks <abrooks2>
2021-11-30 10:29:22 +0000

    safehaven: do not discard all-NA columns. there is no 'project' variable.

--- 05_oneline.R
+++ 05_oneline.R
@@ -1,6 +1,10 @@
 # Oneline combines Day 1 and Discharge data and includes summary variables  --------------------------------------------------------------------
 ## One line per patient
 ### Note: This filters out a very small number of patients who have no Day 1 or Discharge data entered, but do have data on an "Additional days" form
+#### FOR SAFE HAVEN ONLY
+##### 2021-05-27 abrooks removed PatientID from list of columns to delete
+##### 2021-06-29 abrooks removed purrr::discard (three times)
+##### 2021-11-25 abrooks remove project in case safehaven doesn't have it
 oneline = ccp_data %>%
   # Filter for Day 1 event data
   filter(
@@ -8,7 +12,7 @@ oneline = ccp_data %>%
     redcap_event_name == "Day 1 Hospital&ICU Admission (Arm 2: TIER 1)" |
     redcap_event_name == "Day 1 (Arm 3: TIER 2)") %>%
   filter(is.na(redcap_repeat_instrument)) %>%
-  purrr::discard( ~ all(is.na(.))) %>%
+  #purrr::discard( ~ all(is.na(.))) %>%
   # Join to Discharge event data
   full_join(
     ccp_data %>%
@@ -18,7 +22,7 @@ oneline = ccp_data %>%
         redcap_event_name == "Discharge/Death (Arm 2: TIER 1)" |
         redcap_event_name == "Discharge/Death (Arm 3: TIER 2)") %>%
       filter(is.na(redcap_repeat_instrument)) %>%
-      purrr::discard(~ all(is.na(.))) %>%
+      #purrr::discard(~ all(is.na(.))) %>%
       # Deselect variables to avoid duplicate columns when joining
       select(-c(
           # location data
@@ -27,8 +31,8 @@ oneline = ccp_data %>%
           # summary treatment variables
           any_daily_hoterm:status,
           # other duplicate variables
-          dag_id, redcap_data_access_group, project, arm, arm_n)),
+          dag_id, redcap_data_access_group, arm, arm_n)), # XXX abrooks removed project for safehaven
     by = c("subjid"),
     suffix = (c(".day1", ".disch"))) %>%
-  select(-c(redcap_event_name.day1, redcap_event_name.disch)) %>% 
-  purrr::discard(~all(is.na(.)))
+  select(-c(redcap_event_name.day1, redcap_event_name.disch)) #%>% 
+  #purrr::discard(~all(is.na(.)))
